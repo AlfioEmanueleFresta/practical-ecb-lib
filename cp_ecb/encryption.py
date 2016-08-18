b5073e08fa8a778498a7cc7ae5cf0f88f1b4afbe
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2016-08-18 15:29:33 +0100

    Documentted encryption functions

--- cp_ecb/encryption.py
+++ cp_ecb/encryption.py
@@ -49,6 +49,15 @@ def _keystream(key_length, seed):
 
 
 def get_ecb_encrypter(key, block_size=BLOCK_SIZE, key_length=KEY_LENGTH):
+    """
+    Creates a function which can be used to encrypt a bytes literal with ECB
+     encryption and a given key.
+
+    :param key: The key string to use for the encrypter. This will be padded to `key_length` bytes.
+    :param block_size: Optional. The size for the blocks to encrypt. Must be a multiple of 16.
+    :param key_length: Optional. The length of the key. Must be 16, 24 or 32.
+    :return: A function which takes a bytes literal and returns the encrypted bytes literal.
+    """
     key = _ensure_bytes_key(key, length=key_length)
 
     def encrypter(x):
@@ -60,11 +69,26 @@ def get_ecb_encrypter(key, block_size=BLOCK_SIZE, key_length=KEY_LENGTH):
 
 
 def get_ecb_decrypter(key, block_size=BLOCK_SIZE, key_length=KEY_LENGTH):
+    """
+    Creates a function which can be used to decrypt a bytes literal
+     encrypted using with ECB encryption and a given key.
+
+    Please note that the resulting decrypter will accept input data which is not
+     a multiple of the block size (generally 16). This may be the case when using
+     this function with encrypted data which has been truncated (to fit an image, for example).
+     If that's the case it will pad the input data. This means that the last decrypted output
+     block may contain random noise.
+
+    :param key: The key string to use for the decrypter. This will be padded to `key_length` bytes.
+    :param block_size: Optional. The size for the blocks to encrypt. Must be a multiple of 16.
+    :param key_length: Optional. The length of the key. Must be 16, 24 or 32.
+    :return: A function which takes an encrypted bytes literal and returns the decrypted bytes literal.
+    """
     key = _ensure_bytes_key(key, length=key_length)
 
     def decrypter(x):
         cipher = AES.AESCipher(key[:32], AES.MODE_ECB)
-        x = _pad_data(x, block_size=block_size)  # This is a hack, and will loose the last pixels.
+        x = _pad_data(x, block_size=block_size)  # Yes, this is a hack -- read above.
         x = cipher.decrypt(x)
         x = _unpad_data(x, block_size=block_size)
         return x
@@ -73,11 +97,21 @@ def get_ecb_decrypter(key, block_size=BLOCK_SIZE, key_length=KEY_LENGTH):
 
 
 def get_stream_cipher(seed, size=STREAM_LENGTH):
+    """
+    Creates a function which can be used as a stream cipher, and will be able to
+     encrypt *and* decrypt bytes literals.
+
+    :param seed: A string that will be used to seed the stream cipher.
+    :param size: The size (in bytes) of the state of the stream cipher. This must be a divisor of the input size.
+                 It defaults to three bytes -- as that's always the case for uncompressed RGB images.
+    :return: A function which takes a bytes literal and returns a bytes literal.
+    """
     def encrypter(x):
         y = b''
         for data_start_index, key in zip(range(0, len(x), size),
                                          _keystream(key_length=size, seed=seed)):
             y += strxor(x[data_start_index:data_start_index + size], key)
         return y
+
     return encrypter
 
